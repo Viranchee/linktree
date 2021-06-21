9ef3dcf057e64068796fc3883912787359373f09
Viranchee Lotia <viranchee@Ketans-MacBook-Air.local>
2021-06-21 20:29:11 +0530

    Add Links and Prettier

---
+++ src/Data/data.ts
@@ -0,0 +1,15 @@
+export type Linkset = {
+  label: String;
+  link: String;
+  logo: String;
+};
+
+export const data: Linkset[] = [
+      { label: "Github", link: "https://www.github.com/Viranchee", logo: "" }
+      { label: "LinkedIn", link: "https://www.linkedin.com/in/Viranchee", logo: "" }
+      { label: "Twitter", link: "https://www.twitter.com/code_magician", logo: "" }
+
+
+
+
+];
--- src/data.ts
+++
@@ -1,9 +0,0 @@
-export type Linkset = {
-      label: String,
-      link: String,
-      logo: String
-}
-
-export const data: Linkset[] = [{
-      label: "", link: "", logo: ""
-}]
