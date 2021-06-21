5a1ae83e958ca5dea68987cfe3a8ed55d642e631
Danyl Fernandes <androidan1304@gmail.com>
2021-06-21 08:04:06 -0700

    Add tab to prettierrc

--- src/Data/data.ts
+++ src/Data/data.ts
@@ -5,11 +5,7 @@ export type Linkset = {
 };
 
 export const data: Linkset[] = [
-      { label: "Github", link: "https://www.github.com/Viranchee", logo: "" }
-      { label: "LinkedIn", link: "https://www.linkedin.com/in/Viranchee", logo: "" }
-      { label: "Twitter", link: "https://www.twitter.com/code_magician", logo: "" }
-
-
-
-
+  { label: "Github", link: "https://www.github.com/Viranchee", logo: "" },
+  { label: "LinkedIn", link: "https://www.linkedin.com/in/Viranchee", logo: "" },
+  { label: "Twitter", link: "https://www.twitter.com/code_magician", logo: "" }
 ];
